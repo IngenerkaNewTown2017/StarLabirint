87bb6d4ff5518036e963777c9a3c2f363f337181
mabramovsd <beavisabra@yandex.ru>
2018-09-24 11:08:50 +0400

    Обновленные кубы

--- main.cpp
+++ main.cpp
@@ -6,13 +6,13 @@ int point_cuba;//их дохига будет    не забудь даун
 int speed_ball = 35;
 
 int main()
-    {
+{
     txCreateWindow (1280, 720);
     HDC main_menu = txLoadImage ("main_menu.bmp");
     txBitBlt (txDC(), 0, 0, 1280, 720, main_menu, 0, 0);
 
     txDeleteDC (main_menu);
-    }
+}
 
 /*это движение шара вправо */
 void move_ball_rigth()
@@ -41,6 +41,7 @@ void move_ball_up()
     {
         y_ball=y_ball+speed_ball;
     }
+}
 
 
 /*это движение шара вниз */
