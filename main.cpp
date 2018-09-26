b86242928ec69ebf2d3767458dbd8ee46f4f1676
myounger1 <43293278+myounger1@users.noreply.github.com>
2018-09-26 18:22:47 +0400

    кнопка выхода, кнопка Play, пробная игра

--- main.cpp
+++ main.cpp
@@ -1,54 +1,110 @@
+
 #include "TXLib.h"
 
 int x_ball;
 int y_ball;
 int point_cuba;//их дохига будет    не забудь даун
 int speed_ball = 35;
+bool Exit = false;
+bool StartGame = false;
 
 int main()
-{
+    {
     txCreateWindow (1280, 720);
+
     HDC main_menu = txLoadImage ("main_menu.bmp");
-    txBitBlt (txDC(), 0, 0, 1280, 720, main_menu, 0, 0);
+    HDC proba = txLoadImage ("proba.bmp");
+
+
+    while(Exit == false && StartGame == false)
+    {
+        txBegin();
+        txClear();
+        txBitBlt (txDC(), 0, 0, 1280, 720, main_menu, 0, 0);
+
+        if(txMouseButtons () == 1 &&
+            txMouseX () >= 525 &&
+            txMouseX () <= 689 &&
+            txMouseY () >= 480 &&
+            txMouseY () <= 580)
+        {
+            Exit = true;
+        }
+        if(txMouseButtons () == 1 &&
+            txMouseX () >= 517 &&
+            txMouseX () <= 704 &&
+            txMouseY () >= 275 &&
+            txMouseY () <= 368)
+        {
+            StartGame = true;
+        }
+
+        txSleep(10);
+        txEnd();
+    }
+
+    if (StartGame == true)
+    {
+        while(Exit == false)
+        {
+            txBegin();
+            txBitBlt (txDC(), 0, 0, 1280, 720, proba, 0, 0);
+
+            if(txMouseButtons () == 1 &&
+                txMouseX () >= 525 &&
+                txMouseX () <= 689 &&
+                txMouseY () >= 480 &&
+                txMouseY () <= 580)
+            {
+                txBitBlt (txDC(), 400, 400, 1280, 720, proba, 0, 0);
+            }
+            txSleep(10);
+            txEnd();
+        }
+    }
 
     txDeleteDC (main_menu);
+    txDeleteDC (proba);
+    return 0;
 }
 
-/*это движение шара вправо */
+
 void move_ball_rigth()
 {
-    //FIXME Тут скорее if (GetAsyncKeyState(VK_RIGHT)) вместо for.
-    //Или тут выставляешь карту, а шарик движется сам (тогда все правильно, но надо прописать в readme)?
-    for(x_ball=point_cuba; x_ball<=/*другой*/point_cuba; x_ball++)
-    {
-        x_ball=x_ball+speed_ball;
-    }
+/*это движение шара вправо */for(x_ball=point_cuba; x_ball<=/*другой*/point_cuba; x_ball++)
+{
+  x_ball=x_ball+speed_ball;
+}
 }
 
-/*это движение шара влево */
 void move_ball_left()
 {
-    for(x_ball=point_cuba; x_ball<=/*другой*/point_cuba; x_ball++)
-    {
-        x_ball=x_ball-speed_ball;
-    }
+/*это движение шара влево */for(x_ball=point_cuba; x_ball<=/*другой*/point_cuba; x_ball++)
+{
+ x_ball=x_ball-speed_ball;
+}
 }
 
-/*это движение шара вверх */
 void move_ball_up()
 {
-    for(y_ball=point_cuba; y_ball<=/*другой*/point_cuba; y_ball++)
-    {
-        y_ball=y_ball+speed_ball;
-    }
+/*это движение шара вверх */for(y_ball=point_cuba; y_ball<=/*другой*/point_cuba; y_ball++)
+{
+ y_ball=y_ball+speed_ball;
+}
 }
 
-
-/*это движение шара вниз */
 void move_ball_down()
 {
-    for(y_ball=point_cuba; y_ball<=/*другой*/point_cuba; y_ball++)
-    {
-        y_ball=y_ball-speed_ball;
-    }
+/*это движение шара вниз */for(y_ball=point_cuba; y_ball<=/*другой*/point_cuba; y_ball++)
+{
+ y_ball=y_ball-speed_ball;
 }
+}
+
+
+
+
+
+
+
+
