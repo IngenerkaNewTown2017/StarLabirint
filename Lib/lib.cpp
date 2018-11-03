50bbcbecda3feb019ea0bbaa6e2d9a229511bbc5
Mikhail Abramov <beavisabra@yandex.ru>
2018-11-03 21:23:35 +0400

    КОммит Макса

--- Lib/lib.cpp
+++ Lib/lib.cpp
@@ -17,10 +17,14 @@ void risovat_golova(HDC golova,int x ,int y) {
 }
 void risovat_telo(HDC telo,int x ,int y) {
     txBitBlt (txDC(), 205, 395, 169, 168, telo, x, y);
+
 }
 void risovat_fon(HDC fon) {
     txBitBlt (txDC(),0, 0, 1280, 720,fon, 0, 0);
 }
 void vkladka(HDC FONtelo){
     txBitBlt (txDC(),635, 70, 1280, 720,FONtelo, 0, 0);
+}
+void risovat_lico(HDC lico1,int x ,int y) {
+    txBitBlt (txDC(), 205, 295, 150, 50, lico1, x, y);
 }
--- Lib/save.cpp
+++ Lib/save.cpp
@@ -84,7 +84,22 @@ bool ScreenCapture(int x, int y, int width, int height, char *filename){
     HDC hDc = Win32::CreateCompatibleDC(0);
     HBITMAP hBmp = Win32::CreateCompatibleBitmap(GetDC(0), width, height);
     Win32::SelectObject(hDc, hBmp);
-    Win32::BitBlt(hDc, 0, 0, width, height, GetDC(0), x, y, SRCCOPY);
+
+
+    HWND hwndOwner = GetDesktopWindow();
+    WINDOWPLACEMENT rcOwner;
+    GetWindowPlacement(hwndOwner, &rcOwner);
+
+txClear();
+
+char str[100];
+sprintf(str, "%d",  rcOwner.rcNormalPosition.left);  //Writing car_x_coord value to str
+txTextOut(100, 100, str);
+    Sleep(1000);
+
+
+
+    Win32::BitBlt(hDc, 0, 0, width, height, GetDC(0), rcOwner.rcNormalPosition.left + x, rcOwner.rcNormalPosition.top + y, SRCCOPY);
     bool ret = SaveBMPFile(filename, hBmp, hDc, width, height);
     Win32::DeleteObject(hBmp);
 
