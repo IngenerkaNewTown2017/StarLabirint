2668355c6c6d5f1c1c3d957e9bd05389d4ac7a93
IvanTYZplay <42039917+IvanTYZplay@users.noreply.github.com>
2018-12-08 15:05:43 +0300

    Add files via upload

--- zmeika.cpp
+++ zmeika.cpp
@@ -4,10 +4,37 @@ int main()
 {
     txCreateWindow (700,600);
 
+    int xone = 0;
+    int yone = 0;
+
     HDC fon = txLoadImage ("Pictures\\Zmeika\\ito fon.bmp");
-    txBitBlt (txDC(), 0, 0, 700, 600, fon, 0, 0);
     HDC pers = txLoadImage ("Pictures\\Zmeika\\korabl.bmp");
-    txBitBlt (txDC(), 0, 0, 100, 300, pers, 0, 0);
+
+    while(!GetAsyncKeyState('W'))
+     {
+        txBitBlt (txDC(), 0, 0, 700, 600, fon, 0, 0);
+        txTransparentBlt (txDC(), xone, yone, 123, 80, pers, 0, 0,TX_WHITE );
+
+        if (GetAsyncKeyState(VK_LEFT))
+        {
+            xone =  xone - 2;
+        }
+        else if (GetAsyncKeyState(VK_RIGHT))
+        {
+            xone =  xone + 2;
+        }
+        else if (GetAsyncKeyState(VK_UP))
+        {
+            yone =  yone - 2;
+        }
+        else if (GetAsyncKeyState(VK_DOWN))
+        {
+            yone =  yone +2;
+        }
+
+        txSleep(1);
+     }
 
     txDeleteDC(fon);
+    txDeleteDC(pers);
 }
