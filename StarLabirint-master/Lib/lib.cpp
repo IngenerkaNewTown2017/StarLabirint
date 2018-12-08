fa2f084bbc37575ff96556e3ec574cbb3f74f515
MAXER <34571579+MAXer2@users.noreply.github.com>
2018-12-08 15:10:10 +0300

    Работает донат ,значок папки,пустые глаза
    можете попробывать донат) хыы

---
+++ StarLabirint-master/Lib/lib.cpp
@@ -0,0 +1,48 @@
+#include "TXLib.h"
+
+struct ColorButton
+{
+    int x;
+    int x1;
+    int y;
+    int y1;
+    COLORREF color;
+    int pictureX;
+    int pictureY;
+};
+
+//файл для рисования всех частей тел XD
+void risovat_golova(HDC golova,int x ,int y) {
+    txTransparentBlt (txDC(), 220, 255, RAZMER_KARTINKI_GOLOVY, 160, golova, x, y , TX_WHITE);
+}
+void risovat_telo(HDC telo,int x ,int y) {
+    txTransparentBlt (txDC(), 220, 410,RAZMER_KARTINKI_TELA, 180, telo, x, y, TX_WHITE);
+
+}
+void risovat_fon(HDC fon) {
+    txBitBlt (txDC(),0, 0, 1280, 720,fon, 0, 0);
+}
+void vkladka(HDC FONtelo){
+    txBitBlt (txDC(),635, 70, 1280, 720,FONtelo, 0, 0);
+}
+void risovat_lico(HDC lico1,int x ,int y) {
+    txTransparentBlt (txDC(), 238, 342, RAZMER_KARTINKI_EMOJI,37, lico1, x, y, TX_WHITE);
+}
+void reklama (){
+
+       txSetTextAlign (TA_CENTER);
+    txSelectFont("Arial", 95);
+    txSetColor(TX_RED, 5);
+    txDrawText(10, 10, 1000,1000, "нее ворууй");
+    txSleep (95);
+    txSetColor(TX_BLUE, 5);
+    txDrawText(10, 10, 1000,1000, "нее ворууй");
+    txSleep (95);
+    txSetColor(TX_RED, 5);
+    txDrawText(10, 10, 1000,1000, "нее ворууй");
+    txSleep (95);
+    txSetColor(TX_GREEN, 5);
+    txDrawText(10, 10, 1000,1000, "нее ворууй");
+    txSleep (95);
+    txSetColor(TX_YELLOW, 5);
+     txDrawText(10, 10, 1000,1000, "нее ворууй");}
